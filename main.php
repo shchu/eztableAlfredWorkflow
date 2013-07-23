6a0a313647c6698929e86d7706d38a9081d721f7
ShaohanCHU <mark@eztable.com.tw>
2013-07-23 22:53:38 +0800

    add search

--- main.php
+++ main.php
@@ -16,18 +16,18 @@ $restaurantInfo =  json_decode(curl_exec($ch), true);
 // Pass a Bundle ID
 $w = new Workflow();
 $resultArray = array(
-	'uid'          => 'itemuid',
-	'arg'          => 'itemarg',
-	'valid'        => 'yes',
-	'autocomplete' => 'autocomplete',
-	'icon'         => 'icon.png',
-	'title'        => 'EZTABLE restaurant name',
+    'uid'          => 'itemuid',
+    'arg'          => 'itemarg',
+    'valid'        => 'yes',
+    'autocomplete' => 'autocomplete',
+    'icon'         => 'icon.png',
+    'title'        => 'EZTABLE restaurant name',
 );
 
 if ($restaurantInfo['status'] == "OK") {
-	$resultArray['subtitle'] = $restaurantInfo['data']['name'];
+    $resultArray['subtitle'] = $restaurantInfo['data']['name'];
 } else {
-	$resultArray['subtitle'] = 'Not found!';
+    $resultArray['subtitle'] = 'Not found!';
 }
 
 $w->result($resultArray);
---
+++ search.php
@@ -0,0 +1,48 @@
+<?php
+require 'vendor/autoload.php';
+use Alfred\Workflow;
+
+$query = $argv[1];
+
+$w = new Workflow();
+$resultArray = array(
+    'uid'          => 'itemuid',
+    'arg'          => 'itemarg',
+    'valid'        => 'yes',
+    'autocomplete' => 'autocomplete',
+    'icon'         => 'icon.png',
+);
+$result = array();
+
+if (mb_strlen($query) <= 2) {
+    $resultArray['title'] = 'EZTABLE restaurant search';
+    $resultArray['subtitle'] = '...';
+    $w->result($resultArray);
+    echo $w->toXML();
+    exit;
+}
+$ch = curl_init( 'http://api.localhost/v2/search/search_restaurant/2001-01-01/2/?q='.$query);
+$options = array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
+);
+
+curl_setopt_array( $ch, $options );
+$restaurantInfo =  json_decode(curl_exec($ch), true);
+
+if ($restaurantInfo['status'] == "OK" && $restaurantInfo['data']['numFound'] > 0) {
+    $docs = $restaurantInfo['data']['docs'];
+    foreach ($docs as $doc) {
+        $resultTemplate = $resultArray;
+        $resultTemplate['title'] = $doc['name'];
+        $resultTemplate['subtitle'] = $doc['id'];
+        $resultTemplate['url'] = 'http://www.eztable.com/restaurant/'.$doc['id'];
+        $w->result($resultTemplate);
+    }
+} else {
+    $resultArray['title'] = 'EZTABLE restaurant search';
+    $resultArray['subtitle'] = 'Not found!';
+    $w->result($resultArray);
+}
+
+echo $w->toXML();
